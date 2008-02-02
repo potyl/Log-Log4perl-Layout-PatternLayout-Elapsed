2e5fb7c7f27f5f84481438fb8fe5cb17c3e6bc1f
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2008-02-02 12:47:13 +0000

    Documented the code
    git-svn-id: http://svn.potyl.com/repos/cpan/Log-Log4perl-Layout-TimedPatternLayout/trunk@1116 0eeaf444-2d20-0410-987c-9b9cd00b88ca

--- lib/Log/Log4perl/Layout/TimedPatternLayout.pm
+++ lib/Log/Log4perl/Layout/TimedPatternLayout.pm
@@ -1,171 +1,4 @@
-##################################################
 package Log::Log4perl::Layout::TimedPatternLayout;
-##################################################
-
-use 5.006;
-use strict;
-use warnings;
-use Carp;
-
-use base qw(Log::Log4perl::Layout::PatternLayout);
-
-my $CSPECS = 'R' . $Log::Log4perl::Layout::PatternLayout::CSPECS;
-
-##################################################
-sub new {
-##################################################
-    my $type = shift;
-		
-		local $Log::Log4perl::Layout::PatternLayout::CSPECS = $CSPECS;
-		my $self = $type->SUPER::new(@_);
-		$self->{CSPECS} = $CSPECS;
-		$self->{last_time} = undef;
-
-    return $self;
-}
-
-
-##################################################
-sub render {
-##################################################
-    my($self, $message, $category, $priority, $caller_level) = @_;
-
-    $caller_level = 0 unless defined  $caller_level;
-
-    my %info    = ();
-
-    $info{m}    = $message;
-        # See 'define'
-    chomp $info{m} if $self->{message_chompable};
-
-    my @results = ();
-
-    if($self->{info_needed}->{L} or
-       $self->{info_needed}->{F} or
-       $self->{info_needed}->{C} or
-       $self->{info_needed}->{l} or
-       $self->{info_needed}->{M} or
-       0
-      ) {
-        my ($package, $filename, $line, 
-            $subroutine, $hasargs,
-            $wantarray, $evaltext, $is_require, 
-            $hints, $bitmask) = caller($caller_level);
-
-        # If caller() choked because of a whacko caller level,
-        # correct undefined values to '[undef]' in order to prevent 
-        # warning messages when interpolating later
-        unless(defined $bitmask) {
-            for($package, 
-                $filename, $line,
-                $subroutine, $hasargs,
-                $wantarray, $evaltext, $is_require,
-                $hints, $bitmask) {
-                $_ = '[undef]' unless defined $_;
-            }
-        }
-
-        $info{L} = $line;
-        $info{F} = $filename;
-        $info{C} = $package;
-
-        if($self->{info_needed}->{M} or
-           $self->{info_needed}->{l} or
-           0) {
-            # To obtain the name of the subroutine which triggered the 
-            # logger, we need to go one additional level up.
-            my $levels_up = 1; 
-            {
-                $subroutine = (caller($caller_level+$levels_up))[3];
-                    # If we're inside an eval, go up one level further.
-                if(defined $subroutine and
-                   $subroutine eq "(eval)") {
-                    $levels_up++;
-                    redo;
-                }
-            }
-            $subroutine = "main::" unless $subroutine;
-            $info{M} = $subroutine;
-            $info{l} = "$subroutine $filename ($line)";
-        }
-    }
-
-    $info{X} = "[No curlies defined]";
-    $info{x} = Log::Log4perl::NDC->get() if $self->{info_needed}->{x};
-    $info{c} = $category;
-    $info{d} = 1; # Dummy value, corrected later
-    $info{n} = "\n";
-    $info{p} = $priority;
-    $info{P} = $$;
-    $info{H} = $Log::Log4perl::Layout::PatternLayout::HOSTNAME;
-
-    if($self->{info_needed}->{r} || $self->{info_needed}->{R}) {
-        if($Log::Log4perl::Layout::PatternLayout::TIME_HIRES_AVAILABLE) {
-            $info{r} = 
-                int((Time::HiRes::tv_interval ( $Log::Log4perl::Layout::PatternLayout::PROGRAM_START_TIME ))*1000);
-        } else {
-            if(! $Log::Log4perl::Layout::PatternLayout::TIME_HIRES_AVAILABLE_WARNED) {
-                $Log::Log4perl::Layout::PatternLayout::TIME_HIRES_AVAILABLE_WARNED++;
-                # warn "Requested %r pattern without installed Time::HiRes\n";
-            }
-            $info{r} = time() - $Log::Log4perl::Layout::PatternLayout::PROGRAM_START_TIME;
-        }
-    }
-    if($self->{info_needed}->{R}) {
-        my $current_time = $info{r};
-        my $last_time = $self->{last_time} || $current_time;
-        $info{R} = $current_time - $last_time;
-        $self->{last_time} = $current_time;
-    }
-
-        # Stack trace wanted?
-    if($self->{info_needed}->{T}) {
-        my $mess = Carp::longmess(); 
-        chomp($mess);
-        $mess =~ s/(?:\A\s*at.*\n|^\s*Log::Log4perl.*\n|^\s*)//mg;
-        $mess =~ s/\n/, /g;
-        $info{T} = $mess;
-    }
-
-        # As long as they're not implemented yet ..
-    $info{t} = "N/A";
-
-    foreach my $cspec (keys %{$self->{USER_DEFINED_CSPECS}}){
-        next unless $self->{info_needed}->{$cspec};
-        $info{$cspec} = $self->{USER_DEFINED_CSPECS}->{$cspec}->($self, 
-                              $message, $category, $priority, $caller_level+1);
-    }
-
-        # Iterate over all info fields on the stack
-    for my $e (@{$self->{stack}}) {
-        my($op, $curlies) = @$e;
-        if(exists $info{$op}) {
-            my $result = $info{$op};
-            if($curlies) {
-                $result = $self->curly_action($op, $curlies, $info{$op});
-            } else {
-                # just for %d
-                if($op eq 'd') {
-                    $result = $info{$op}->format($self->{time_function}->());
-                }
-            }
-            $result = "[undef]" unless defined $result;
-            push @results, $result;
-        } else {
-            warn "Format %'$op' not implemented (yet)";
-            push @results, "FORMAT-ERROR";
-        }
-    }
-
-    #print STDERR "sprintf $self->{printformat}--$results[0]--\n";
-
-    return (sprintf $self->{printformat}, @results);
-}
-
-
-1;
-
-__END__
 
 =head1 NAME
 
@@ -304,4 +137,185 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
 at your option, any later version of Perl 5 you may have available.
 
+=head1 METHODS
+
+This module defined the following methods.
+
 =cut
+
+use 5.006;
+
+use strict;
+use warnings;
+
+use Carp;
+
+use base qw(Log::Log4perl::Layout::PatternLayout);
+
+my $CSPECS = 'R' . $Log::Log4perl::Layout::PatternLayout::CSPECS;
+
+=head2 new
+
+This is the class constructor it's simply hack's with the instance created by
+the parent. The arguments are the same as 
+C<Log::Log4perl::Layout::PatternLayout->new()>.
+
+=cut
+
+sub new {
+
+    my $type = shift;
+		
+		local $Log::Log4perl::Layout::PatternLayout::CSPECS = $CSPECS;
+		my $self = $type->SUPER::new(@_);
+		$self->{CSPECS} = $CSPECS;
+		$self->{last_time} = undef;
+
+    return $self;
+}
+
+
+=head2 render
+
+A copie of C<Log::Log4perl::Layout::PatternLayout::render()> with the code
+needed in order to add the fucntionality required for the format C<%R>.
+
+=cut
+
+sub render {
+    my($self, $message, $category, $priority, $caller_level) = @_;
+
+    $caller_level = 0 unless defined $caller_level;
+
+    my %info    = ();
+
+    $info{m}    = $message;
+        # See 'define'
+    chomp $info{m} if $self->{message_chompable};
+
+    my @results = ();
+
+    if($self->{info_needed}->{L} or
+       $self->{info_needed}->{F} or
+       $self->{info_needed}->{C} or
+       $self->{info_needed}->{l} or
+       $self->{info_needed}->{M} or
+       0
+      ) {
+        my ($package, $filename, $line, 
+            $subroutine, $hasargs,
+            $wantarray, $evaltext, $is_require, 
+            $hints, $bitmask) = caller($caller_level);
+
+        # If caller() choked because of a whacko caller level,
+        # correct undefined values to '[undef]' in order to prevent 
+        # warning messages when interpolating later
+        unless(defined $bitmask) {
+            for($package, 
+                $filename, $line,
+                $subroutine, $hasargs,
+                $wantarray, $evaltext, $is_require,
+                $hints, $bitmask) {
+                $_ = '[undef]' unless defined $_;
+            }
+        }
+
+        $info{L} = $line;
+        $info{F} = $filename;
+        $info{C} = $package;
+
+        if($self->{info_needed}->{M} or
+           $self->{info_needed}->{l} or
+           0) {
+            # To obtain the name of the subroutine which triggered the 
+            # logger, we need to go one additional level up.
+            my $levels_up = 1; 
+            {
+                $subroutine = (caller($caller_level+$levels_up))[3];
+                    # If we're inside an eval, go up one level further.
+                if(defined $subroutine and
+                   $subroutine eq "(eval)") {
+                    $levels_up++;
+                    redo;
+                }
+            }
+            $subroutine = "main::" unless $subroutine;
+            $info{M} = $subroutine;
+            $info{l} = "$subroutine $filename ($line)";
+        }
+    }
+
+    $info{X} = "[No curlies defined]";
+    $info{x} = Log::Log4perl::NDC->get() if $self->{info_needed}->{x};
+    $info{c} = $category;
+    $info{d} = 1; # Dummy value, corrected later
+    $info{n} = "\n";
+    $info{p} = $priority;
+    $info{P} = $$;
+    $info{H} = $Log::Log4perl::Layout::PatternLayout::HOSTNAME;
+
+    if($self->{info_needed}->{r} || $self->{info_needed}->{R}) {
+        if($Log::Log4perl::Layout::PatternLayout::TIME_HIRES_AVAILABLE) {
+            $info{r} = 
+                int((Time::HiRes::tv_interval ( $Log::Log4perl::Layout::PatternLayout::PROGRAM_START_TIME ))*1000);
+        } else {
+            if(! $Log::Log4perl::Layout::PatternLayout::TIME_HIRES_AVAILABLE_WARNED) {
+                $Log::Log4perl::Layout::PatternLayout::TIME_HIRES_AVAILABLE_WARNED++;
+                # warn "Requested %r pattern without installed Time::HiRes\n";
+            }
+            $info{r} = time() - $Log::Log4perl::Layout::PatternLayout::PROGRAM_START_TIME;
+        }
+    }
+    if($self->{info_needed}->{R}) {
+        my $current_time = $info{r};
+        my $last_time = $self->{last_time} || $current_time;
+        $info{R} = $current_time - $last_time;
+        $self->{last_time} = $current_time;
+    }
+
+        # Stack trace wanted?
+    if($self->{info_needed}->{T}) {
+        my $mess = Carp::longmess(); 
+        chomp($mess);
+        $mess =~ s/(?:\A\s*at.*\n|^\s*Log::Log4perl.*\n|^\s*)//mg;
+        $mess =~ s/\n/, /g;
+        $info{T} = $mess;
+    }
+
+        # As long as they're not implemented yet ..
+    $info{t} = "N/A";
+
+    foreach my $cspec (keys %{$self->{USER_DEFINED_CSPECS}}){
+        next unless $self->{info_needed}->{$cspec};
+        $info{$cspec} = $self->{USER_DEFINED_CSPECS}->{$cspec}->($self, 
+                              $message, $category, $priority, $caller_level+1);
+    }
+
+        # Iterate over all info fields on the stack
+    for my $e (@{$self->{stack}}) {
+        my($op, $curlies) = @$e;
+        if(exists $info{$op}) {
+            my $result = $info{$op};
+            if($curlies) {
+                $result = $self->curly_action($op, $curlies, $info{$op});
+            } else {
+                # just for %d
+                if($op eq 'd') {
+                    $result = $info{$op}->format($self->{time_function}->());
+                }
+            }
+            $result = "[undef]" unless defined $result;
+            push @results, $result;
+        } else {
+            warn "Format %'$op' not implemented (yet)";
+            push @results, "FORMAT-ERROR";
+        }
+    }
+
+    #print STDERR "sprintf $self->{printformat}--$results[0]--\n";
+
+    return (sprintf $self->{printformat}, @results);
+}
+
+
+1;
